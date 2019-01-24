5fc2333c8d6fed2b01fb656c06df738a8d8fbe51
lordUser <sapega-89@mail.ru>
2019-01-24 19:00:25 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
