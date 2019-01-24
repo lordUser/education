8fbe2f80e981e6a4db60fd1c3899277f79637c96
lordUser <sapega-89@mail.ru>
2019-01-24 18:42:29 +0300

    Update education_extras.py

--- core/templatetags/education_extras.py
+++ core/templatetags/education_extras.py
@@ -6,4 +6,4 @@ register = template.Library()
 
 @register.simple_tag()
 def admin_chenge(obj):
-    return reverse("admin:%s_%s_change" % (obj._meta.app_label, obj._meta.model_name), args=(obj.id,))
+    return reverse("admin:{}_{}_change".format(obj._meta.app_label, obj._meta.model_name), args=(obj.id,))
